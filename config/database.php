3751f192a3555501741fa167886f9025b1db8c72
19muno <juanmunozgarcia745@gmail.com>
2025-07-27 12:02:02 -0500

    actualizacion

--- config/database.php
+++ config/database.php
@@ -1,6 +1,15 @@
 <?php
 
 use Illuminate\Support\Str;
+if (env('DATABASE_URL')) {
+    $url = parse_url(env('DATABASE_URL'));
+    putenv('DB_HOST=' . $url['host']);
+    putenv('DB_PORT=' . $url['port'] ?? 3306);
+    putenv('DB_DATABASE=' . ltrim($url['path'], '/'));
+    putenv('DB_USERNAME=' . $url['user']);
+    putenv('DB_PASSWORD=' . $url['pass']);
+}
+
 
 return [
 
